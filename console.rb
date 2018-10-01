aad4b17dfd804662f209399ab5076641107baba4
Mayumi Yoshida <mayumiyoshida@mayumis-mbp.lan>
2018-10-01 13:09:24 -0700

    changes to console file

--- console.rb
+++ console.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require "pry"
-require_relative "recurlysite"
-site= new_site
+load "credentials.rb"
+require_relative "spec/spec_helper.rb"
+site= new_session(BASEURL, USER, PASSWORD)
 puts "Pry session started.\n"
 binding.pry
